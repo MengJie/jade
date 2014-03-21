81859e49b0129af15ae0255eeff1658a73b1bb42
MengJie <inmouse@gmail.com>
2014-03-22 00:43:39 +0800

    initialize some component
    * using glfw3 lib build by vs2010
    * init the lua state
    * make a simple logger.

---
+++ src/core/logger.cpp
@@ -0,0 +1,118 @@
+//========================================================================
+// JADE 0.1
+//------------------------------------------------------------------------
+// Copyright (c) 2014 Jie Meng <inmouse@gmail.com>
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would
+//    be appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not
+//    be misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source
+//    distribution.
+//
+//========================================================================
+
+#include "logger.hpp"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+USING_JADE_NS
+
+const int CLogger::BUF_SIZE = 4096;
+CLogger * CLogger::instance = 0;
+char CLogger::buffer[BUF_SIZE];
+
+CLogger * CLogger::getInstance()
+{
+    if (0 == instance) {
+        instance = new CLogger();
+    }
+    return instance;
+}
+
+CLogger::CLogger()
+{
+}
+
+void CLogger::debug(const char * fmt, ...)
+{
+    int writed = 0;
+
+    writed += _snprintf_s(buffer + writed, BUF_SIZE - writed,
+            _TRUNCATE, "[DEBUG] ");
+
+    va_list argp;
+    va_start(argp, fmt);
+    writed += vsnprintf(buffer + writed, BUF_SIZE - writed,
+            fmt, argp);
+    va_end(argp);
+
+    flush();
+}
+
+void CLogger::info(const char * fmt, ...)
+{
+    int writed = 0;
+
+    writed += _snprintf_s(buffer + writed, BUF_SIZE - writed,
+            _TRUNCATE, "[INFO] ");
+
+    va_list argp;
+    va_start(argp, fmt);
+    writed += vsnprintf(buffer + writed, BUF_SIZE - writed,
+            fmt, argp);
+    va_end(argp);
+
+    flush();
+}
+
+void CLogger::warn(const char * fmt, ...)
+{
+    int writed = 0;
+
+    writed += _snprintf_s(buffer + writed, BUF_SIZE - writed,
+            _TRUNCATE, "[WARN] ");
+
+    va_list argp;
+    va_start(argp, fmt);
+    writed += vsnprintf(buffer + writed, BUF_SIZE - writed,
+            fmt, argp);
+    va_end(argp);
+
+    flush();
+}
+
+void CLogger::error(const char * fmt, ...)
+{
+    int writed = 0;
+
+    writed += _snprintf_s(buffer + writed, BUF_SIZE - writed,
+            _TRUNCATE, "[ERROR] ");
+
+    va_list argp;
+    va_start(argp, fmt);
+    writed += vsnprintf(buffer + writed, BUF_SIZE - writed,
+            fmt, argp);
+    va_end(argp);
+
+    flush();
+}
+
+void CLogger::flush()
+{
+    fputs(buffer, stderr);
+}
+
---
+++ src/entry/win32/main.cpp
@@ -0,0 +1,84 @@
+#include <glfw/glfw3.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "luastate.hpp"
+
+using namespace jade;
+
+static void 
+keyCallback(GLFWwindow* window, int key, int scancode,
+	int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+static void
+errorCallback(int error, const char * desc)
+{
+	fputs(desc, stderr);
+}
+
+int main(void)
+{
+	CLuaState::getInstance();
+
+    GLFWwindow* window;
+
+	glfwSetErrorCallback(errorCallback);
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+	glfwSetKeyCallback(window, keyCallback);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+		float ratio;
+		int width, height;
+
+		glfwGetFramebufferSize(window, &width, &height);
+		ratio = width / (float)height;
+
+		glViewport(0, 0, width, height);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glMatrixMode(GL_PROJECTION);
+		glLoadIdentity();
+		glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+
+		glBegin(GL_TRIANGLES);
+        glColor3f(1.f, 0.f, 0.f);
+        glVertex3f(-0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 1.f, 0.f);
+        glVertex3f(0.6f, -0.4f, 0.f);
+        glColor3f(0.f, 0.f, 1.f);
+        glVertex3f(0.f, 0.6f, 0.f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+	glfwSwapBuffers(window);
+    glfwTerminate();
+    return 0;
+}
